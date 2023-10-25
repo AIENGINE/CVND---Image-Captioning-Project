eda859a145f5ff9779a32c7ae791dd9378979d5d
ali <engalidanish@gmail.com>
2023-10-25 23:01:32 +0200

    remove last entry from caption to adjust for image embedding

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+import torch.nn.functional as F
 
 
 class EncoderCNN(nn.Module):
@@ -22,7 +23,7 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, captions_shape, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, feature_shape, num_layers=1):
         super(DecoderRNN, self).__init__()
         
         self.dict_size = vocab_size
@@ -31,35 +32,79 @@ class DecoderRNN(nn.Module):
         self.hidden_dim = hidden_size
         self.embedding_dim = embed_size # output feature shape from CNN
         self.feature_embeddings = nn.Embedding(self.dict_size, self.embedding_dim)
-        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=num_layers, dropout=self.dropout_prob, batch_first=True)
-        # self.dropout = nn.Dropout(self.dropout_prob)
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, dropout=self.dropout_prob, num_layers=num_layers, batch_first=True)
+        self.dropout = nn.Dropout(self.dropout_prob)
         self.linear = nn.Linear(self.hidden_dim, self.dict_size)
 
-        self.batch_dim = captions_shape[0]
-        self.hidden_state = self.init_hidden(self.batch_dim)
+        self.batch_dim = feature_shape
+        # self.hidden_state = self.init_hidden(self.batch_dim)
+        self.init_weights_linear_layer()
+
+    def init_weights_linear_layer(self):
+        ''' Initialize weights for fully connected layer '''
+        self.feature_embeddings.weight.data.uniform_(-0.1, 0.1)
+        # Set bias tensor to all zeros
+        self.linear.bias.data.fill_(0)
+        # FC weights as random uniform
+        self.linear.weight.data.uniform_(-1, 1)
+
     
     def init_hidden(self, batch_dim):
 
         # The axes dimensions are (n_layers, batch_size, hidden_dim)
-        return (torch.zeros(self.lstm_layers, batch_dim, self.hidden_dim),
-                torch.zeros(self.lstm_layers, batch_dim, self.hidden_dim))
+        return (torch.zeros(self.lstm_layers, batch_dim, self.hidden_dim).cuda(),
+                torch.zeros(self.lstm_layers, batch_dim, self.hidden_dim).cuda())
     
     def forward(self, features, captions):
+        
+        # to concat image tensor 1 char has to be dropped
+        captions = captions[:, :-1] 
         embeddings = self.feature_embeddings(captions)
-        print(embeddings.shape)
-        
-        lstm_out, self.hidden_state = self.lstm(features.unsqueeze(dim=1), self.hidden_state)
-        print(f"lstm_out shape: {lstm_out.shape} h and c: {self.hidden_state[0].shape}, {self.hidden_state[1].shape}")
-        lstm_out, self.hidden_state = self.lstm(embeddings.reshape(self.batch_dim, -1, self.embedding_dim), self.hidden_state)
-        print(f"lstm_out shape: {lstm_out.shape} h and c: {self.hidden_state[0].shape}, {self.hidden_state[1].shape}")
-        
-        linear_out = self.linear(lstm_out.reshape(-1, self.hidden_dim))
+        # print(embeddings.shape)
+        hc = self.init_hidden(self.batch_dim)
+        feature_embed = torch.cat((features.unsqueeze(dim=1), embeddings), dim=1)
+        lstm_out, (h, c) = self.lstm(feature_embed, hc)
+       
+        # lstm_out, (h, c) = self.lstm(features.unsqueeze(dim=1), hc)
+        # print(f"lstm_out shape: {lstm_out.shape} h and c: {self.hidden_state[0].shape}, {self.hidden_state[1].shape}")
+        # lstm_out, (h, c) = self.lstm(embeddings.reshape(self.batch_dim, -1, self.embedding_dim), (h, c))
+        # print(f"lstm_out shape: {lstm_out.shape} h and c: {self.hidden_state[0].shape}, {self.hidden_state[1].shape}")
+       
+        linear_out_drop = self.dropout(lstm_out)
+        linear_out_drop = self.linear(linear_out_drop.reshape(-1, self.hidden_dim))
+        x = linear_out_drop.reshape(self.batch_dim, -1, self.dict_size)
 
-        return linear_out.reshape(self.batch_dim, -1, self.dict_size)
-        
-
+        return x
 
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        print(f"input dim: {inputs.shape}")
+        sent_idx = []
+        hc = states
+        top_word_idx = torch.zeros(1, 20)
+        for idx in range(max_len):
+        # while True:
+
+            lstm_out, hc = self.lstm(inputs, hc)
+            # print(f"lstm_out dim: {lstm_out.shape}, hidden shape: {hc[0].shape}, {hc[1].shape}")
+            linear_out = self.linear(lstm_out.reshape(-1, self.hidden_dim))
+            # tag_scores = F.log_softmax(linear_out, dim=1)
+            pred, top_word_idx = torch.max(linear_out, dim=1)
+            print(f"pred score: {pred}, word idx: {top_word_idx}")
+            inputs = self.feature_embeddings(top_word_idx)
+            inputs = inputs.unsqueeze(dim=1)
+            top_word_idx = top_word_idx.squeeze().to("cpu").tolist()
+            # if top_word_idx == 1:
+            sent_idx.append(top_word_idx)
+                # break    
+
+        # print(f"top word idx shape: {top_word_idx.shape}")
+        # top_word_idx = top_word_idx.squeeze(dim=0)
+        # top_word_idx = top_word_idx.to("cpu")
+        # top_word_idx = top_word_idx.tolist()
+
+        # print(f"top word idx shape: {len(top_word_idx)}")
+        print(f"len of sent idx: {len(sent_idx)}")
+        return sent_idx
+
